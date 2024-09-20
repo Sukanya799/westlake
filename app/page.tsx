8507d96cb6904b62f875fb8829a117a58d31d8ae
websines <to.subh03@gmail.com>
2024-09-21 00:49:18 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import { AnimatedBeamMultipleOutputDemo } from "@/components/AnimatedBeam";
 import { AnimatedShinyTextDemo } from "@/components/AnimatedShinyText";
+import BoxRevealDemo from "@/components/BoxReveal";
 import { CoverDemo } from "@/components/Cover";
+import BoxReveal from "@/components/magicui/box-reveal";
+import NumberTicker from "@/components/magicui/number-ticker";
+import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
+import LetsMakeThingsHappenSection from "@/components/ui/MakeThingsHappen";
+import { ScrollBasedVelocityDemo } from "@/components/ui/scroll-based-velodity-main";
+import { ShootingStarsAndStarsBackgroundDemo } from "@/components/ui/ShootingStars";
+import { WordPullUpDemo } from "@/components/ui/wordpullup";
+import { IconStarFilled } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
+import { PiCheckBold } from "react-icons/pi";
 import { Link as ScrollLink, Element } from "react-scroll";
 
 const services = [
@@ -134,6 +145,7 @@ export default function Home() {
             px-10
             md:px-16
       md:text-xl
+      text-sm
       hover:bg-[#abcbff] 
       rounded-[6px]
       border-2 
@@ -147,24 +159,242 @@ export default function Home() {
             >
               Book a Call
             </Link>
-            <Link
-              href={"/showcase"}
+            <ScrollLink
+              to="services"
               className="
               bg-white
    py-3 
    px-10
    md:px-16
       md:text-xl
+      text-sm
         border-4
         border-black
         rounded-[6px]
         hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
             >
-              Showcase
-            </Link>
+              Services
+            </ScrollLink>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Design
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Development
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Marketing
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Strategy
+              </p>
+            </BoxReveal>
+          </div>
+
+          <div className="md:flex items-center justify-between gap-y-4 my-10 gap-x-28 mx-auto">
+            <div className="md:w-2/5">
+              <h1 className="text-2xl font-medium text-gray-600 w-4/5">
+                Trusted by fast moving brands worldwide
+              </h1>
+
+              <div className="flex my-6 gap-x-5 w-full">
+                <div>
+                  <h1 className="text-blue-500 text-3xl md:text-5xl">
+                    <NumberTicker value={1000} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Happy Clients
+                    </p>
+                  </h1>
+                </div>
+
+                <div className="w-px bg-gray-300 self-stretch"></div>
+
+                <div className="flex-1 min-w-0">
+                  <h1 className="text-blue-500 text-3xl md:text-5xl whitespace-nowrap overflow-hidden">
+                    <NumberTicker value={100} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Projects Completed
+                    </p>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <section className="overflow-hidden mt-10 md:w-4/5">
+              <InfiniteMovingLogos
+                speed="slow"
+                direction="left"
+                items={[
+                  {
+                    logo: "/logo/logo.webp",
+                    name: "Logo",
+                  },
+                  {
+                    logo: "/logo/logo.webp",
+                    name: "Logo",
+                  },
+                ]}
+              />
+            </section>
           </div>
         </div>
       </main>
+
+      <Element name="services">
+        <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto ">
+          <h1>
+            <WordPullUpDemo />
+          </h1>
+          <p className="md:text-center py-4 md:w-1/2 mx-auto text-xl md:text-2xl text-gray-500">
+            All of our services are designed to help your business stand out
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
+            {services.map((service) => (
+              <div
+                key={service.title}
+                className="flex flex-col justify-between h-full space-y-4 text-center bg-gray-100 p-4 cursor-pointer hover:scale-105 transition-transform rounded-md"
+              >
+                <Image
+                  src={service.icon}
+                  width={10000}
+                  height={10000}
+                  className="object-contain bg-gray-100 p-4 w-full h-40 rounded-md"
+                  alt="image"
+                />
+                <h1 className="text-xl font-medium">{service.title}</h1>
+                <p className="text-gray-500">{service.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Element>
+
+      <section className="py-20">
+        <ScrollBasedVelocityDemo />
+      </section>
+
+      <Element name="process">
+        <main className="md:px-0 mx-6 md:mx-auto">
+          <h1 className="text-3xl md:text-5xl md:text-center font-medium flex items-center gap-x-2 mx-auto justify-center">
+            Our{" "}
+            <span className="text-blue-500 flex gap-x-1 items-center">
+              {" "}
+              <Image
+                src={"/icons/squiggle.svg"}
+                width={10000}
+                height={10000}
+                className="w-6"
+                alt="image"
+              />
+              Creative
+              <Image
+                src={"/icons/star.svg"}
+                width={10000}
+                height={10000}
+                className="w-6 mb-8"
+                alt="image"
+              />
+            </span>{" "}
+            Process
+          </h1>
+
+          <p
+            className="text-center 
+          py-4 md:w-1/2 mx-auto 
+          text-xl md:text-2xl text-gray-500"
+          >
+            All of our services are designed to help your business to get
+            noticed.
+          </p>
+
+          <div className="flex flex-col md:flex-row items-center justify-center w-full md:w-1/2 mx-auto">
+            <div className="w-full md:w-1/2 order-2 md:order-1">
+              <AnimatedBeamMultipleOutputDemo />
+            </div>
+            <div className="w-full md:w-1/2 order-1 md:order-2 md:ml-0">
+              <BoxRevealDemo />
+            </div>
+          </div>
+        </main>
+      </Element>
+      <section>
+        <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
+          <Image
+            src={"/logo/logo.webp"}
+            width={10000}
+            height={10000}
+            className=" md:w-1/3 rounded-md"
+            alt="image"
+          />
+          <div className="flex flex-col gap-y-5 md:w-1/2">
+            <h1 className="text-lg md:text-2xl ">
+              &quot;We&apos;ve been working with Bird for over 2 years and
+              they&apos;ve been amazing to work with. They&apos;ve helped us
+              grow our business and we couldn&apos;t be happier with the
+              results. &quot;
+            </h1>
+            <div className="flex items-center gap-x-1">
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+            </div>
+
+            <span className="text-xl font-medium">
+              Jordan, Brisson <br />
+              CEO, Atlas Massage
+            </span>
+          </div>
+        </main>
+      </section>
+
+      <Element name="guarentees">
+        <ShootingStarsAndStarsBackgroundDemo />
+      </Element>
+
+      <section className="my-10 md:py-20 xl:w-4/5 2xl:w-[68%] md:mx-auto">
+        <LetsMakeThingsHappenSection />
+      </section>
+
+      <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
+        <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
+          <h1 className="text-3xl md:text-5xl font-medium ">
+            <Image
+              src={"/logo/logo.webp"}
+              width={10000}
+              height={10000}
+              className="w-40"
+              alt="image"
+            />{" "}
+          </h1>
+          <p className="text-left  text-xl  text-gray-500">519-200-5000</p>
+          <p className="text-left  text-xl  text-gray-500">
+            admin@westlake.com
+          </p>
+        </div>
+
+        <div className="flex md:justify-center gap-x-4 mt-10">
+          © 2025 westLake. All Rights Reserved.
+          <Link href="/" className="text-blue-500">
+            Privacy Policy
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 }
