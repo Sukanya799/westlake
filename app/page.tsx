9f1696bfbb80aede001bf8de8a03abd2952b8f38
Sukanya <sukanyamondal799@gmail.com>
2024-11-19 11:20:34 +0530

    gy

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import LetsMakeThingsHappenSection from "@/components/ui/MakeThingsHappen";
+
 import {
   Shield,
   Cloud,
